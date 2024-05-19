1008a3c872e67d6660c8d03308e2a25f21dc5956
Waldheimer <dewaldheimer@googlemail.com>
2024-05-19 16:19:00 +0200

    Project initialization

---
+++ Appointmenting.API/Infrastructure/Database/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Appointmenting.API.Infrastructure.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
